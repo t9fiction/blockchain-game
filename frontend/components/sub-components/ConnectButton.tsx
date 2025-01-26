2c8335315e074ab3b180ba06731c7c682bef86da
t9fiction <sohail.sohailishaq@gmail.com>
2025-01-27 03:04:57 +0500

    added comment for the expect error

--- frontend/components/sub-components/ConnectButton.tsx
+++ frontend/components/sub-components/ConnectButton.tsx
@@ -1,33 +1,32 @@
 'use client'
 import React from 'react';
-import { useAppKitAccount, useDisconnect } from '@reown/appkit/react'
+import { useAppKitAccount, useDisconnect } from '@reown/appkit/react';
 import { useAppKit } from '@reown/appkit/react';
 
 export default function ConnectButton() {
     const { open } = useAppKit();
-    const { address, isConnected, caipAddress, status } = useAppKitAccount()
-    const { disconnect } = useDisconnect()
+    const { isConnected } = useAppKitAccount();
+    const { disconnect } = useDisconnect();
 
     const handleOpen = () => {
-        open() // Opens the AppKit interface or modal
+        open(); // Opens the AppKit interface or modal
     };
-
-    const handleClose = () => {
-        disconnect(); // Closes the AppKit interface or modal
-    };
-    // return <appkit-button />
 
     return (
         <div>
-            {!isConnected ?
-                <div onClick={handleOpen} className="text-secondary hover:text-gray-900 uppercase border border-spacing-1 border-white cursor-pointer ease-in-out duration-300 rounded-sm py-2 px-4 border-foreground">
+            {!isConnected ? (
+                <div
+                    onClick={handleOpen}
+                    className="text-secondary hover:text-gray-900 uppercase border border-spacing-1 border-white cursor-pointer ease-in-out duration-300 rounded-sm py-2 px-4 border-foreground"
+                >
                     Connect
                 </div>
-                : <div className="text-secondary border border-spacing-1 border-foreground ">
-                    {/* @ts-expect-error */}
+            ) : (
+                <div className="text-secondary border border-spacing-1 border-foreground">
+                    {/* @ts-expect-error This is a known issue with the appkit-button type definition */}
                     <appkit-button />
                 </div>
-            }
+            )}
         </div>
     );
 }
