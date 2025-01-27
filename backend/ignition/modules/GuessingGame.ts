ae41ff7187b7dfa3576a100378acc690c3eb791c
t9fiction <sohail.sohailishaq@gmail.com>
2025-01-27 15:13:59 +0500

    working arbitrumSepolia and sepolia with verification

--- backend/ignition/modules/GuessingGame.ts
+++ backend/ignition/modules/GuessingGame.ts
@@ -4,6 +4,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const _tokenAddress = '0xFE4fEE4298E67536bE875585CA0b031Aad078782';
+// const _tokenAddress = '0x71f0F6182F9dDc39BC20C073C8687C48B108BB43';
 
 const GuessingGameModule = buildModule("GuessingGameModule", (m) => {
   const rewardToken = m.getParameter("rewardToken", _tokenAddress);
