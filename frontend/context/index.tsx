0fa9768a30c59b40a5f864836a6af07e0d2fcac6
t9fiction <sohail.sohailishaq@gmail.com>
2025-01-27 18:57:47 +0500

    again removed arbitrum

--- frontend/context/index.tsx
+++ frontend/context/index.tsx
@@ -3,7 +3,7 @@
 import { wagmiAdapter, projectId } from '@/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createAppKit } from '@reown/appkit/react'
-import { sepolia, arbitrumSepolia } from '@reown/appkit/networks'
+import { sepolia } from '@reown/appkit/networks'
 import React, { type ReactNode } from 'react'
 import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
 
