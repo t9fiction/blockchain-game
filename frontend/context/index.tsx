9fe0830f4eea9fd51c053e33826b7045a9d480a0
t9fiction <sohail.sohailishaq@gmail.com>
2025-01-27 18:57:30 +0500

    arbitrum removed

--- frontend/context/index.tsx
+++ frontend/context/index.tsx
@@ -26,7 +26,7 @@ const metadata = {
 const modal = createAppKit({
   adapters: [wagmiAdapter],
   projectId,
-  networks: [sepolia, arbitrumSepolia],
+  networks: [sepolia],
   defaultNetwork: sepolia,
   metadata: metadata,
   features: {
